ed810bdefc4ba20e9f122311d0d1b2b2ac052330
Luo Boming <14920359+fur0ut0@users.noreply.github.com>
2025-04-13 00:38:09 +0900

    add gtest

---
+++ lib/number_theory/test/prime.cpp
@@ -0,0 +1,8 @@
+#include "../prime.hpp"
+#include <gtest/gtest.h>
+
+TEST(is_prime, positive_small) { ASSERT_EQ(is_prime(99991LL), true); }
+TEST(is_prime, positive_medium) { ASSERT_EQ(is_prime(524287LL), true); }
+TEST(is_prime, positive_large) { ASSERT_EQ(is_prime(472194691LL), true); }
+TEST(is_prime, negative_0) { ASSERT_EQ(is_prime(2013LL), false); }
+TEST(is_prime, negative_1) { ASSERT_EQ(is_prime(2023LL), false); }
