0310c65ca4017867a99e8d55e63ecf7823099734
Luo Boming <14920359+fur0ut0@users.noreply.github.com>
2025-04-19 19:23:48 +0900

    add comments

--- lib/number_theory/test/prime.cpp
+++ lib/number_theory/test/prime.cpp
@@ -6,3 +6,5 @@ TEST(is_prime, positive_medium) { ASSERT_EQ(is_prime(524287LL), true); }
 TEST(is_prime, positive_large) { ASSERT_EQ(is_prime(472194691LL), true); }
 TEST(is_prime, negative_0) { ASSERT_EQ(is_prime(2013LL), false); }
 TEST(is_prime, negative_1) { ASSERT_EQ(is_prime(2023LL), false); }
+
+// TODO: sieve
